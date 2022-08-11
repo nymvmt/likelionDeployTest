fbb078698178377d1dca2f631311c016aad44148
Kim NaYoung <99931269+nymvmt@users.noreply.github.com>
2022-08-11 20:59:29 +0900

    Delete Session16/Session16_HW directory

--- Session16/Session16_HW/artherapy/accounts/urls.py
+++
@@ -1,9 +0,0 @@
-from accounts import views
-from django.urls import path
-
-urlpatterns = [
-    path('signup/', views.signup, name='signup'),
-    path('login/', views.login, name='login'),
-    path('logout/', views.logout, name='logout'),
-    # path('main/', views.main, name='main'),
-]
