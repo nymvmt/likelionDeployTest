cec1488c3e87a0395b457ccb5c025aed04edb7e9
Kim NaYoung <99931269+nymvmt@users.noreply.github.com>
2022-08-11 21:00:26 +0900

    Delete Session9_django_review/Session9_HW directory

--- Session9_django_review/Session9_HW/project/app/models.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Post(models.Model):
-    title = models.CharField(max_length=50)
-    content = models.TextField()
-    counting = models.PositiveIntegerField(default=0)
